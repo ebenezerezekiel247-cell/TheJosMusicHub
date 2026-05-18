4478ef75838437ae7a1820549ee72210cce1d06d
ebenezerezekiel247-cell <ebenezerezekiel247@gmail.com>
2026-05-19 00:12:53 +0100

    Add Tailwind CSS configuration file

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./app/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        brand: "#0EA5E9"
+      }
+    }
+  },
+  plugins: []
+}
